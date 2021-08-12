d4157cc11ccf72283dba4518903c308b64381db2
Hebina Lee <62586511+hebinalee@users.noreply.github.com>
2021-08-12 23:06:11 +0900

    Delete normalize_sfc.m

--- utils/normalize_sfc.m
+++
@@ -1,8 +0,0 @@
-function norm_sfc = normalize_sfc(sfc)
-norm_sfc = sfc;
-steps = size(norm_sfc,3);
-for i = 1 : steps
-	stepfc = norm_sfc(:,:,i);
-    norm_sfc(:,:,i) = (stepfc - mean(stepfc(:))) / std(stepfc(:));
-end
-end
