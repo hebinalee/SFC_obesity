9608c96264ac724dd267c8ac25e3d57f58f5507d
Hebina Lee <62586511+hebinalee@users.noreply.github.com>
2021-10-01 18:57:31 +0900

    Create binarize_conn.m

---
+++ utils/binarize_conn.m
@@ -0,0 +1,27 @@
+function binconn = binarize_conn(conn)
+th = 5;
+Nroi = size(conn, 1);
+binconn = zeros(Nroi, Nroi);
+
+W = conn;
+if isequal(W,W.')	% if symmetric matrix
+    W=triu(W);      % ensure symmetry is preserved
+    ud=2;           % halve number of removed links
+else
+    ud=1;
+end
+    
+ind=find(W);                            % find all links
+E=sortrows([ind W(ind)], -2);           % sort by magnitude
+en=round((Nroi^2-Nroi)*0.01*th/ud);     % number of links to be preserved. the value in front of '/ud' will be changed
+W(E(en+1:end,1))=0;                     % apply threshold
+    
+% if symmetric, reconstruct symmetry
+if ud==2
+    W=W+W.';
+end
+
+% binarize
+W=double(W~=0);
+binconn = W;
+end
