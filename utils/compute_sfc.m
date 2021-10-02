b2dbd308dd4015084c18bc98273f36f00de49ef2
Hebina Lee <62586511+hebinalee@users.noreply.github.com>
2021-10-02 13:57:11 +0900

    Update compute_sfc.m

--- utils/compute_sfc.m
+++ utils/compute_sfc.m
@@ -1,4 +1,12 @@
 function sfc = compute_sfc(binconn, Nstep)
+% 
+% This function counts the number of all possible paths that connect different brain regions with specific step distances
+% and return binarized connectivity matrix
+% 
+% Inputs :  conn - Weighted connectivity matrix (Nroi x Nroi)
+% Outputs:  binconn - Binarized connectivity matrix (Nroi x Nroi x Nstep)
+% 
+
 [sfc, ~, ~] = findwalks(binconn);
 sfc = sfc(:, :, 1:Nstep);
 Nroi = size(binconn, 1);
